f3f176c773ea5e2b3a2a522bb1ca208489bcc973
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-05-26 11:14:51 -0300

    Project created

---
+++ src/test/java/com/tech_challenge_04/customers/CustomersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
